87aee2d1432e69de2a58da66b3ffb5cabb1e44be
Jon <jambler24@gmail.com>
2018-08-03 16:39:11 +0200

    Added process_samples.py

---
+++ process_samples.py
@@ -0,0 +1,49 @@
+import subprocess, argparse
+
+
+def main(inFile):
+	"""
+
+	:param inFile:
+	:param fastq_dir:
+	:return:
+	"""
+	input_file = open(inFile)
+	new_sample_sheet = inFile.split('.')[0] + '_new.csv'
+	output_file = open(new_sample_sheet, 'w')
+
+	for line in input_file:
+		line = line.split(',')
+		if line[4].split('.')[-1] != 'fq' and line[4].split('.')[-1] != 'fastq' and line[4].split('.')[-1] != 'R1':
+			print('SRA detected')
+			print(line[4])
+
+			#subprocess.run(['fastq-dump', '-X', '5', '-I', '--split-files', line[4]])
+			subprocess.run(['fastq-dump', '-I', '--split-files', line[4]])
+
+			print(line)
+			new_line = line[0] + ',' + line[1] + ',' + line[2] + ',' + line[3] + ',' + fastq_dir + line[
+				4] + '_1.fastq' + ',' + fastq_dir + line[4] + '_2.fastq' + '\n'
+			output_file.write(new_line)
+
+		else:
+			new_line = ",".join(map(str, line))
+			output_file.write(new_line)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+		description='''This tool downloads any reads from the SRA in the sample sheet, and makes a new one with 
+		the paths.
+		''',  epilog="""Version 0.1""")
+
+	parser.add_argument('-i', '--input_file', type=str, help='Input sample file')
+	parser.add_argument('-f', '--fastq_dir', type=str, help='Directory where reads from the SRA will be moved')
+
+	args = parser.parse_args()
+
+	inFile = args.input_file
+	fastq_dir = args.fastq_dir
+
+	main(inFile)
+
