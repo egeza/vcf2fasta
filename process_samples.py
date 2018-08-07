9c156b9302de7f49f51e455e8f2b211fbaf6c96b
Jon <jambler24@gmail.com>
2018-08-07 14:49:04 +0200

    Removed trailing newline char in SRA entries

--- process_samples.py
+++ process_samples.py
@@ -19,9 +19,9 @@ def main(inFile, fastq_dir, fast_test_mode):
 			print(line[4])
 
 			if fast_test_mode:
-				subprocess.run(['fastq-dump', '-X', '5', '-I', '--split-files', line[4]])
+				subprocess.run(['fastq-dump', '-X', '5', '-I', '--split-files', line[4].rstrip()])
 			else:
-				subprocess.run(['fastq-dump', '-I', '--split-files', line[4]])
+				subprocess.run(['fastq-dump', '-I', '--split-files', line[4].rstrip()])
 
 			print(line)
 			new_line = line[0] + ',' + line[1] + ',' + line[2] + ',' + line[3] + ',' + fastq_dir + line[
